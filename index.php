137c53c4b0c082b24cb60caff83a242f8c7344d0
default <devin.boucher@stu.ocsb.ca>
2025-02-26 19:49:19 +0000

    added PHP website

--- index.php
+++ index.php
@@ -1,9 +1,38 @@
 <!DOCTYPE html>
-  <html>
+<!-- ICS2O-Unit1-05-HTML-MDL -->
+<html lang="en-ca">
   <head>
-    <title>Title</title>
+    <meta charset="utf-8" />
+    <meta name="description" content="My first JS webpage" />
+    <meta name="keywords" content="mths, icd2o" />
+    <meta name="author" content="Devin Boucher" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    />
+    <link
+      rel="stylesheet"
+      href="https://code.getmdl.io/1.3.0/material.deep_purple-indigo.min.css"
+    />
+    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+    <link rel="manifest" href="site.webmanifest" />
+    <title>Hello, World! with php</title>
   </head>
   <body>
-    <?php echo '<p>Hello, World!, in PHP</p>'; ?>
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+      <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+          <span class="mdl-layout-title">Hello, World! with PHP</span>
+        </div>
+      </header>
+      <main class="mdl-layout__content">
+          <?php echo '<p>Hello, World!</p>'; ?>
+      </main>
+    </div>
   </body>
 </html>
