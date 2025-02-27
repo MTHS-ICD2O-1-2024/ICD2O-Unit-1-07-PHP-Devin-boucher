e42675ab63e366cfcfc12f9a05e4f0ca50c5e577
default <devin.boucher@stu.ocsb.ca>
2025-02-27 19:55:54 +0000

    added PHP website

--- index.php
+++ index.php
@@ -25,13 +25,18 @@
   <body>
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-      <header class="mdl-layout__header">
-        <div class="mdl-layout__header-row">
-          <span class="mdl-layout-title">Hello, World! with PHP</span>
-        </div>
-      </header>
-      <main class="mdl-layout__content">
-          <?php echo '<p>Hello, World!</p>'; ?>
+    <header class="mdl-layout__header">
+    <div class="mdl-layout__header-row">
+    <span class="mdl-layout-title">Hello, World! with PHP</span>
+    </div>
+    </header>
+    <main class="mdl-layout__content">
+    <?php echo '<p>Hello, World!</p>'; ?>
+    <img src="./images/download.jpeg" />
+        <!-- Simple Textfield for integers-->
+        <form action="#">
+          <br />
+        </form>
       </main>
     </div>
   </body>
